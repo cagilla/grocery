640726ea88f87c86a36acca8c3dfdaa7c7b91d18
Chris Gillanders <cgillanders@confinedphoenix.com>
2012-10-27 08:30:16 -0400

    used best in place for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  unit         :string(255)
+#  num_required :integer
+#  num_onhand   :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  location     :string(255)
+#
+
 class Item < ActiveRecord::Base
-  attr_accessible :name, :num_onhand, :num_required, :unit
+  attr_accessible :name, :num_onhand, :num_required, :unit, :location
+  
 end
--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  unit         :string(255)
+#  num_required :integer
+#  num_onhand   :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  location     :string(255)
+#
+
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase

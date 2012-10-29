800161847d359ded2201cacd906f9df4a3e80b59
Chris Gillanders <cgillanders@confinedphoenix.com>
2012-10-29 15:34:33 -0400

    moved / route to items#index

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ resources :users
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'static_pages#home'
+  root :to => 'items#index'
 
   # See how all your routes lay out with "rake routes"
 
